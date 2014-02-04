1c5317f2484521e077b282ce1c4585f32472766b
cjauqui <cjauqui@hotmail.com>
2014-02-04 17:27:07 +0100

    [ENH] Arduino server

---
+++ ArduinoUDPServer.ino
@@ -0,0 +1,105 @@
+/*
+  UDPSendReceive.pde:
+ This sketch receives UDP message strings, prints them to the serial port
+ and sends an "acknowledge" string back to the sender
+ 
+ A Processing sketch is included at the end of file that can be used to send 
+ and received messages for testing with a computer.
+ 
+ created 21 Aug 2010
+ by Michael Margolis
+ 
+ This code is in the public domain.
+ */
+
+
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet.h>
+#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(192, 168, 1, 100);
+
+unsigned int localPort = 8888;      // local port to listen on
+
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+
+void setup()
+{
+  // start the Ethernet and UDP:
+  Ethernet.begin(mac,ip);
+  Udp.begin(localPort);
+
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if(packetSize)
+  {
+    IPAddress remoteAddress = Udp.remoteIP();
+    int       port          = Udp.remotePort();
+    
+    printInfo(packetSize, remoteAddress, port);
+    
+    //buffer to hold incoming packet
+    char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+    // read the packet into packetBufffer
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    
+    printMessage(packetBuffer);
+    
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(remoteAddress, port);
+    
+    char  answerBuffer[14 + packetSize + 1];
+    char ReplyBuffer[] = "acknowledged: ";       // a string to send back
+    strcat(answerBuffer, ReplyBuffer);
+    strcat(answerBuffer, packetBuffer);
+    strcat(answerBuffer, '\0');
+  
+    Udp.write(answerBuffer);
+    Udp.endPacket();
+  }
+  delay(10);
+}
+
+
+void printInfo(int packetSize, IPAddress remoteAddress, int port)
+{
+  Serial.print("Received packet of size ");
+  Serial.println(packetSize);
+  Serial.print("From ");
+  for (int i =0; i < 4; i++)
+  {
+    Serial.print(remoteAddress[i], DEC);
+    if (i < 3)
+    {
+      Serial.print(".");
+    }
+  }
+  Serial.print(", port ");
+  Serial.println(port);
+}
+
+void printMessage(char* packetBuffer)
+{
+  Serial.println("Contents:");
+  Serial.println(*packetBuffer);  
+}
+
+
+int proccessMessage(char* answer)
+{
+    
+  return 1;
+}
